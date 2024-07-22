1a788be3edc2f3160c6277aa65a43d2ea7fe3784
Adham-kareem <adhamkareem97@gmail.com>
2024-07-22 20:03:21 +0530

    Add files via upload

---
+++ voteage.java
@@ -0,0 +1,11 @@
+public class voteage {
+    public static void main(String[] args) {
+        int age=21;
+        if(age>=18){
+            System.out.println("you are eligible to vote");
+        }else{
+            System.out.println("you are not eligible to vote");
+        }
+    }
+    
+}
